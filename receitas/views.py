4cfc8daf77e6798496a7114db113e8c3b30c0dd9
alexml07 <alex.ml.07@gmail.com>
2025-12-02 21:42:50 -0300

    251202 2142 Fim da Aula23

--- receitas/views.py
+++ receitas/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from receitas.fabrica.receitasFake import gen_fake_receita
 # from django.http import HttpResponse
+
+# Create your views here.
 var_texto='SENAI'
 
 def home(request):
     return render(request, 'page/home.html', context={
         'nome': var_texto,
+        'receitas': [gen_fake_receita()
+        for _ in range(50)],
     })
 
 def receita(request):
-    return render(request, 'page/receita-view.html', context={'nome': var_texto,})
+    return render(request, 'page/receita-view.html', context={'nome': var_texto, 'receita': gen_fake_receita()})
 
 # Create your views here.
